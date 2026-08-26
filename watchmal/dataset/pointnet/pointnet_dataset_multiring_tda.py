c1e243b0bb1f9fd40c6c595399d6669c3a34fefa
Katie Shaw <ks2922@login1.int.fir.alliancecan.ca>
2026-08-26 03:59:47 -0700

    Snapshot for HX2 handoff: V8 180ep, ResNet 60ep/groupnorm, gated TDA multiring

--- watchmal/dataset/pointnet/pointnet_dataset_multiring_tda.py
+++ watchmal/dataset/pointnet/pointnet_dataset_multiring_tda.py
@@ -64,3 +64,38 @@ class PointNetDatasetMultiRingTDAMergeDist(PointNetDataset):
         else:
             data_dict["merge_distance"] = np.float32(self.merge_h5["merge_distance"][pos])
         return data_dict
+
+
+class PointNetDatasetMultiRingTDAPerRingNpy(PointNetDataset):
+    """
+    Same as PointNetDatasetMultiRingTDAPerRing, but loads TDA features from
+    pre-converted .npy files with mmap_mode='r' instead of h5py.Dataset
+    indexing -- tests whether h5py's per-call read overhead is the actual
+    bottleneck versus the gate mechanism itself.
+    """
+    def __init__(self, h5file, geometry_file, tda_features_prefix, use_times=True,
+                 use_orientations=False, n_points=2000, transforms=None, use_memmap=True):
+        super().__init__(h5file, geometry_file, use_times, use_orientations,
+                          n_points, transforms, use_memmap)
+        self.tda_features_prefix = tda_features_prefix
+        self.tda_ring0 = None
+        self.tda_ring1 = None
+        self.idx_to_pos = None
+
+    def initialize(self):
+        super().initialize()
+        self.tda_ring0 = np.load(f"{self.tda_features_prefix}_ring0.npy", mmap_mode='r')
+        self.tda_ring1 = np.load(f"{self.tda_features_prefix}_ring1.npy", mmap_mode='r')
+        event_idxs = np.load(f"{self.tda_features_prefix}_event_idxs.npy")
+        self.idx_to_pos = {int(idx): pos for pos, idx in enumerate(event_idxs)}
+
+    def __getitem__(self, item):
+        data_dict = super().__getitem__(item)
+        pos = self.idx_to_pos.get(int(item))
+        if pos is None:
+            data_dict["tda_features_ring0"] = np.zeros(self.tda_ring0.shape[1], dtype=np.float32)
+            data_dict["tda_features_ring1"] = np.zeros(self.tda_ring1.shape[1], dtype=np.float32)
+        else:
+            data_dict["tda_features_ring0"] = np.array(self.tda_ring0[pos], dtype=np.float32)
+            data_dict["tda_features_ring1"] = np.array(self.tda_ring1[pos], dtype=np.float32)
+        return data_dict
